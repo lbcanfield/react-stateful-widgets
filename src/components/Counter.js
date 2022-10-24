4833b1a4bfca7ebbb144d8e99b3d0560366d3fc6
lbcanfield <lewie.b.canfield@gmail.com>
2022-10-24 17:17:24 -0500

    Moods.js tasks complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -80,7 +80,7 @@ export default function Counter()
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is even {/* STEP 3 */}
+        Number {count} is {(count %2) ? 'odd' : 'even'} {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
